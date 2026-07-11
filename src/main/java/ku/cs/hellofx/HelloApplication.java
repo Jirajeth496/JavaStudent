34cc8ff6df36762420ca4dc14ff914ac9b72fab0
Nile <ninejiragte50@gmail.com>
2026-07-11 23:20:28 +0700

    in class

---
+++ src/main/java/ku/cs/hellofx/HelloApplication.java
@@ -0,0 +1,24 @@
+package ku.cs.hellofx;
+
+import ku.cs.services.FXRouter;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXRouter.bind(this, stage, "Hello World");
+        configRoute();
+        FXRouter.goTo("student-list");
+    }
+
+    public static void configRoute()
+    {
+        String viewPath = "ku/cs/views/";
+        FXRouter.when("hello", viewPath + "hello-view.fxml");
+        FXRouter.when("student-profile", viewPath + "student.fxml");
+        FXRouter.when("student-list", viewPath + "student-list.fxml");
+    }
+}
